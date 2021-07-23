22628a93635781f72a5dd60c879e44e86c93d05f
Tiagoclobo <87878305+Tiagoclobo@users.noreply.github.com>
2021-07-23 17:42:51 -0300

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // diretiva para o pré-processador
+
+using namespace std;
+
+int main(){
+	//comandos do programa
+	cout <<"Tiago Garcia Lobo-211027320 " << endl;
+	return 0;
+}
+
